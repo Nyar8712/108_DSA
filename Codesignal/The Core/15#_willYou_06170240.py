b16fbed409a4c9bbc7a2d24450fe7832f0ab418e
Nyar8712 <55571112+Nyar8712@users.noreply.github.com>
2020-01-10 13:14:39 +0800

    Create 15#_willYou_06170240.py

---
+++ Codesignal/The Core/15#_willYou_06170240.py
@@ -0,0 +1,11 @@
+def willYou(young, beautiful, loved):
+    while loved == True:
+        if young == True and beautiful == True:
+            return False
+        else:
+            return True
+    while loved == False:
+        if young == True and beautiful == True:
+            return True
+        else:
+            return False
